bbf65055611138304593829869c6db4b7265bb6c
Joshua Li <oasislrjjrl@gmail.com>
2016-02-22 10:37:17 +1100

    Used D3 functions in lib.

--- controllers/toiletsState.js
+++ controllers/toiletsState.js
@@ -41,41 +41,10 @@ ElasticApp.controller('stateController', function ($scope, $http) {
               $scope.error = null;
 
               // Compose pie chart using D3
-              $scope.options = {
-                chart: {
-                  type: 'pieChart',
-                  height: 500,
-                  x: function(d){return d.key;},
-                  y: function(d){return d.y;},
-                  showLabels: false,
-                  duration: 500,
-                  labelThreshold: 0.01,
-                  labelSunbeamLayout: true,
-                  legend: {
-                    margin: {
-                      top: 5,
-                      right: 35,
-                      bottom: 5,
-                      left: 0
-                    }
-                  }
-                }
-              };
+              $scope.options = composePieChartOptions();
 
               var aggregations = data.responses[0].aggregations;
-              var key;
-              for (key in aggregations) {
-                if (aggregations[key].hasOwnProperty("buckets")) {
-                  var buckets = aggregations[key].buckets;
-                }
-              }
-
-              var bucket;
-              for (bucket in buckets) {
-                buckets[bucket].y = buckets[bucket].doc_count;
-                delete buckets[bucket].doc_count;
-              }
-              $scope.data = buckets;
+              $scope.data = composePieChartData(aggregations);
             });
       })
 });
